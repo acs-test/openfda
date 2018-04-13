4c917e5291ec8e50a172ae15423011ff2af604fe
Alvaro del Castillo <acs@bitergia.com>
2018-04-13 06:50:00 +0200

    [check_repos] Deep refactoring before adding the testing of the final project

--- tests/check_repos.py
+++ tests/check_repos.py
@@ -36,9 +36,10 @@ GITHUB_REPOS_API = 'https://api.github.com/repos'
 GITHUB_USERS_API = 'https://api.github.com/users'
 OPENFDA_REPO = "openfda"  # Name of the repository with the practices
 PRACTICES_DIRS = ['openfda-1', 'openfda-2', 'openfda-3', 'openfda-4']
-REPORT_FILE = 'report.json'
+STUDENT_RESULTS_FILE = 'report.json'
 
 class Report():
+    """ This Report is useful to track the activity of students in GitHub """
     students = 0
     repos_found = 0
     repos_not_found = 0
@@ -49,18 +50,155 @@ class Report():
     repo_ko_students = []
     repo_main_not_found = 0
     repo_main_not_found_students = []
-    students_data = {}
+    students_res = {}
 
     @staticmethod
-    def do_report_json():
+    def check_repo(gh_login):
+        """
+        Check is a github user has the openfda repo and its contents
+
+        :param gh_login: github login for the user to test
+        :return:
+        """
+
+        def do_checks(gh_repo):
+            """
+
+            :rtype: Boolean with the result of the checks
+            """
+
+            check = False
+
+            print("Checking", gh_repo)
+
+            # Checks that practices exists
+            practices_pending = list(PRACTICES_DIRS)
+            res = send_github(gh_repo + "/contents")
+            res.raise_for_status()
+
+            for content in res.json():
+                if content['type'] == "dir":
+                    try:
+                        practices_pending.remove(content['name'])
+                    except ValueError:
+                        pass
+
+            if not practices_pending:
+                print("All practices found")
+                check = True
+            else:
+                print("Practices not found for %s: %s" % (gh_repo, practices_pending))
+                check = False
+
+            # Add the practices pending data
+            Report.students_res[gh_login]['practices_pending'] = practices_pending
+            Report.students_res[gh_login]['number_practices_found'] = len(PRACTICES_DIRS) - len(practices_pending)
+
+            # Check last commit date: get last commit and get the date
+            commits_url = GITHUB_REPOS_API + "/" + gh_login + "/" + OPENFDA_REPO + "/commits/master"
+            res = send_github(commits_url)
+            last_commit_date = res.json()['commit']['committer']['date']
+
+            Report.students_res[gh_login]['last_commit_date'] = last_commit_date
+            print("Last commit date", last_commit_date)
+
+            # Check number of commits from the list of contributors
+            contribs_url = GITHUB_REPOS_API + "/" + gh_login + "/" + OPENFDA_REPO + "/contributors"
+            res = send_github(contribs_url)
+            if len(res.json()) > 1:
+                print("A student with contributors!", contribs_url)
+            commits = 0
+            for contributor in res.json():
+                commits += int(contributor['contributions'])
+
+            Report.students_res[gh_login]['number_commits'] = commits
+            print("Number of commits", commits)
+
+            return check
+
+        repos_url = GITHUB_USERS_API + "/" + gh_login + "/repos"
+        page = 1
+
+        check_repos = False  # Check if there are repositories
+        check_main_repo = False  # Checks for the main repository
+        check_main_repo_not_found = True  # Check if the main repository exists
+
+        while True:
+            try:
+                res = send_github(repos_url + "?page=%i" % page)
+                page += 1  # Prepare for next page request
+                res.raise_for_status()
+                Report.repos_found += 1
+                check_repos = True
+            except Exception as ex:
+                print(ex)
+                print("Can not find", repos_url)
+                Report.repos_not_found += 1
+                Report.repos_not_found_students.append(gh_login)
+                break
+
+            repos_dict = res.json()
+
+            if not repos_dict:
+                break
+
+            for repo in repos_dict:
+                if repo['name'] != OPENFDA_REPO:
+                    continue
+                else:
+                    print("Found repo %s for %s" % (repo['name'], gh_login))
+                    Report.students_res[gh_login]['url'] = repo['html_url']
+                    check_main_repo_not_found = False
+                    check_main_repo = do_checks(repo['url'])
+                    break
+
+        if check_main_repo_not_found:
+            print("Not found %s for %s" % (OPENFDA_REPO, gh_login))
+            Report.repo_main_not_found += 1
+            Report.repo_main_not_found_students.append(gh_login)
+
+        return check_repos and check_main_repo
+
+    @staticmethod
+    def fetch_report_data(students_data):
+        """
+        Fetch the data from GitHub to generate the report
+
+        :param students_data: dict with the logins in github of the students
+        :return: None, it fills the report data
+        """
+
+        for name, gh_login in students_data.items():
+            Report.students += 1
+            Report.students_res[gh_login] = {
+                'last_commit_date': None,
+                'number_commits': 0,
+                'number_practices_found': 0,
+                'practices_pending': [],
+                'project_found': False,
+                "url": None
+            }
+            print("Checking repo for", name, gh_login)
+            try:
+                if not Report.check_repo(gh_login):
+                    print("%s (%s) repo KO" % (name, gh_login))
+                    Report.repo_ko += 1
+                    Report.repo_ko_students.append(name)
+                else:
+                    print("%s (%s) repo OK" % (name, gh_login))
+                    Report.repo_ok += 1
+                    Report.repo_ok_students.append(name)
+            except Exception as ex:
+                print("Can't not get data from %s (%s)" % (name, gh_login))
+                print(ex)
+
+    @staticmethod
+    def show():
         """ Generate the report from a report dict"""
 
         # List ordered by last modification
-        data = Report.students_data
-
-        # login with data as value
         login_date = {}
-        for login, value in Report.students_data.items():
+        for login, value in Report.students_res.items():
             login_date[login] = value['last_commit_date'] if value['last_commit_date'] else ''
         login_date_list = sorted(login_date.items(), key=operator.itemgetter(1), reverse=True)
         print("\nTop Dates\n--------")
@@ -68,14 +206,14 @@ class Report():
             print("{0:25} {1}".format(entry[0], entry[1]))
 
         # List ordered by number of commits
-        top_commits = {login: value['number_commits'] for (login, value) in Report.students_data.items()}
+        top_commits = {login: value['number_commits'] for (login, value) in Report.students_res.items()}
         top_commits_list = sorted(top_commits.items(), key=operator.itemgetter(1), reverse=True)
         print("\nTop Commits\n-----------")
         for entry in top_commits_list:
             print("{0:25} {1}".format(entry[0], entry[1]))
 
         # List ordered by number of practices
-        top_practices = {login: value['number_practices_found'] for (login, value) in Report.students_data.items()}
+        top_practices = {login: value['number_practices_found'] for (login, value) in Report.students_res.items()}
         top_practices_list = sorted(top_practices.items(), key=operator.itemgetter(1), reverse=True)
         print("\nTop Practices\n-----------")
         for entry in top_practices_list:
@@ -83,24 +221,31 @@ class Report():
 
 
     @staticmethod
-    def do_report(report_file=None):
-        if not report_file:
+    def do_report(students_data=None, students_res=None):
+        """
+        Generate the report for the activity of the students in GitHub
+
+        :param students_data: dict with the logins of the students
+        :param students_res: an already generated report with students results
+        :return:
+        """
+        if not students_res:
+            # If the data is not collected yet, collect it
+            Report.fetch_report_data(students_data)
+
             print("Total students", Report.students)
             print("Total repos not found", Report.repos_not_found, Report.repos_not_found_students)
             print("Total %s no found: %i %s" % (OPENFDA_REPO, Report.repo_main_not_found, Report.repo_main_not_found_students))
 
-            print(json.dumps(Report.students_data, indent=True, sort_keys=True))
+            print(json.dumps(Report.students_res, indent=True, sort_keys=True))
 
-            freport = open(REPORT_FILE, "w")
-            json.dump(Report.students_data, freport, indent=True, sort_keys=True)
+            freport = open(STUDENT_RESULTS_FILE, "w")
+            json.dump(Report.students_res, freport, indent=True, sort_keys=True)
             freport.close()
         else:
-            print("Generating the report from", REPORT_FILE)
-            freport = open(REPORT_FILE)
-            Report.students_data = json.load(freport)
-            freport.close()
+            Report.students_res = students_res
 
-        Report.do_report_json()
+        Report.show()
 
 
 def get_params():
@@ -115,141 +260,21 @@ def get_params():
 def send_github(url, headers=None):
     headers = {'Authorization': 'token ' + args.token}
     res = requests.get(url, headers=headers)
-    res.raise_for_status()
-    return res
-
-
-def check_repo(gh_login):
-    """ Check is a github user has the openfda repo and it is valid"""
-
-    def do_checks(gh_repo):
-        """
-
-        :rtype: Boolean with the result of the checks
-        """
-
-        check = False
-
-        print("Checking", gh_repo)
-
-        # Checks that practices exists
-        practices_pending = list(PRACTICES_DIRS)
-        res = send_github(gh_repo + "/contents")
+    try:
         res.raise_for_status()
-
-        for content in res.json():
-            if content['type'] == "dir":
-                try:
-                    practices_pending.remove(content['name'])
-                except ValueError:
-                    pass
-
-        if not practices_pending:
-            print("All practices found")
-            check = True
-        else:
-            print("Practices not found for %s: %s" % (gh_repo, practices_pending))
-            check = False
-
-        # Add the practices pending data
-        Report.students_data[gh_login]['practices_pending'] = practices_pending
-        Report.students_data[gh_login]['number_practices_found'] = len(PRACTICES_DIRS) - len(practices_pending)
-
-        # Check last commit date: get last commit and get the date
-        commits_url = GITHUB_REPOS_API + "/" + gh_login + "/" + OPENFDA_REPO + "/commits/master"
-        res = send_github(commits_url)
-        last_commit_date = res.json()['commit']['committer']['date']
-
-        Report.students_data[gh_login]['last_commit_date'] = last_commit_date
-        print("Last commit date", last_commit_date)
-
-        # Check number of commits from the list of contributors
-        contribs_url = GITHUB_REPOS_API + "/" + gh_login + "/" + OPENFDA_REPO + "/contributors"
-        res = send_github(contribs_url)
-        if len(res.json()) > 1:
-            print("A student with contributors!", contribs_url)
-        commits = 0
-        for contributor in res.json():
-            commits += int(contributor['contributions'])
-
-        Report.students_data[gh_login]['number_commits'] = commits
-        print("Number of commits", commits)
-
-        return check
-
-    repos_url = GITHUB_USERS_API + "/" + gh_login + "/repos"
-    page = 1
-
-    check_repos = False  # Check if there are repositories
-    check_main_repo = False  # Checks for the main repository
-    check_main_repo_not_found = True  # Check if the main repository exists
-
-    while True:
-        try:
-            res = send_github(repos_url + "?page=%i" % page)
-            page += 1  # Prepare for next page request
-            res.raise_for_status()
-            Report.repos_found += 1
-            check_repos = True
-        except Exception as ex:
-            print(ex)
-            print("Can not find", repos_url)
-            Report.repos_not_found += 1
-            Report.repos_not_found_students.append(gh_login)
-            break
-
-        repos_dict = res.json()
-
-        if not repos_dict:
-            break
-
-        for repo in repos_dict:
-            if repo['name'] != OPENFDA_REPO:
-                continue
-            else:
-                print("Found repo %s for %s" % (repo['name'], gh_login))
-                Report.students_data[gh_login]['url'] = repo['html_url']
-                check_main_repo_not_found = False
-                check_main_repo = do_checks(repo['url'])
-                break
-
-    if check_main_repo_not_found:
-        print("Not found %s for %s" % (OPENFDA_REPO, gh_login))
-        Report.repo_main_not_found += 1
-        Report.repo_main_not_found_students.append(gh_login)
-
-    return check_repos and check_main_repo
+    except requests.exceptions.HTTPError:
+        print("Can not get repository data (is empty?) from", url)
+    return res
 
 
 if __name__ == '__main__':
 
     args = get_params()
 
-    if os.path.isfile(REPORT_FILE):
-        print("Using the already generated report file", REPORT_FILE)
-        Report.do_report(REPORT_FILE)
-        sys.exit(0)
-
-    fstudents = open(args.students_data)
-
-    for name, gh_login in json.load(fstudents).items():
-        Report.students += 1
-        Report.students_data[gh_login] = {
-            'last_commit_date': None,
-            'number_commits': 0,
-            'number_practices_found': 0,
-            'practices_pending': [],
-            'project_found': False,
-            "url": None
-        }
-        print("Checking repo for", name, gh_login)
-        if not check_repo(gh_login):
-            print("%s (%s) repo KO" % (name, gh_login))
-            Report.repo_ko += 1
-            Report.repo_ko_students.append(name)
-        else:
-            print("%s (%s) repo OK" % (name, gh_login))
-            Report.repo_ok += 1
-            Report.repo_ok_students.append(name)
-
-    Report.do_report()
+    if os.path.isfile(STUDENT_RESULTS_FILE):
+        print("Using the already generated students results", STUDENT_RESULTS_FILE)
+        with open(STUDENT_RESULTS_FILE) as file_results_data:
+            Report.do_report(students_res=json.load(file_results_data))
+    else:
+        with open(args.students_data) as file_student_data:
+            Report.do_report(students_data=json.load(file_student_data))
